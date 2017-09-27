10523ffc3c3a1d5b37fdf578d20c9b97deb8bcf4
CAMARG <gomes.camargo22@gmail.com>
2017-09-27 12:59:51 -0300

    parte 3 concluida

--- src/app/posts/posts-mock.ts
+++ src/app/posts/posts-mock.ts
@@ -3,5 +3,5 @@ import { PostsModel } from './posts.model';
 export const POSTS: PostsModel[] = [
     {id: 1, nomePessoa: 'Fulano de tal', texto: "Olá tudo bem?", qtdLikes: 4},
     {id: 2, nomePessoa: 'Ciclano', texto:"Bom dia!", qtdLikes: 3},
-    {id: 3, nomePessoa: 'Escatamaquio', texto:"Sejam todo ben-vindos!", qtdLikes: 56 }
+    {id: 3, nomePessoa: 'Escatamaquio', texto:"Sejam todos bem-vindos!", qtdLikes: 56 }
 ];
